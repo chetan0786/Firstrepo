ba0567935c892df9f1d8e3fb8314d4b44ae055be
chetan0786 <devganchetan6@gmail.com>
2019-02-22 10:03:03 +0530

    INITIAL COMMIT

---
+++ primesum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	int i,j,k;
+	int flag1=0;
+	int flag2=0;
+	for(i=0;i<=a/2;i++)
+	{
+		flag1=0;
+		flag2=0;
+		for(j=2;j<i/2;j++)
+		{
+			if(i%j==0)
+			{
+				flag1=1;
+				break;
+			}
+		}
+		for(j=2;j<(a-i)/2;j++)
+		{
+			if((a-i)%j==0)
+			{
+				flag2=1;
+				break;
+			}
+		}
+		if(flag1==0&&flag2==0)
+		{
+			cout<<i<<"+"<<a-i<<"="<<a<<endl;
+		}
+		
+		
+	}
+}
